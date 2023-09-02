35ae270ed54c265d2b1442bdefa394d1efa42350
Vinícius Almeida <almeidabarrosdf@gmail.com>
2023-09-02 16:51:06 -0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from generate_files import generate_files
 from variables import *
-import traceback
+from send_mail import send_mail_outlook, check_emails_available_outlook, client
 from send_mail import send_mail
 
 # Amount of times they we will try to generate the files 
@@ -18,25 +18,26 @@ for i in range(tries):
 
 
 if is_generation_successful:
+    outlook = client.Dispatch('Outlook.Application')
+
+
     dict_conditions = {
         "always" : True,
     }
 
-
     for email in list_emails:
-        credentials = list_credentials[email['from']]
-        smtp_config = list_emails_providers[credentials['provider']]['smtp']
-        if dict_conditions[email['condition']]:
-            send_mail(
-                send_from = email['from'],
-                send_to = email['mailto'],
-                subject = email['subject'],
-                body = open(os.path.join(email_messages_path,email['text']),encoding="UTF-8").read(),
-                attachments = [list_pdf_files[0]],
-                smtp_url = smtp_config['server'],
-                port = smtp_config['port'],
-                username = email['from'], 
-                password = credentials['password']
-            )
+        if check_emails_available_outlook(outlook, email['from']):
+            if dict_conditions[email['condition']]:
+                send_mail_outlook(
+                    outlook = outlook,
+                    send_from = email['from'],
+                    send_to = email['mailto'],
+                    subject = email['subject'],
+                    body = open(os.path.join(email_messages_path,email['text']),encoding="UTF-8").read(),
+                    attachments = [list_pdf_files[0]],
+                )
+                print(f"{email['from']}: File was sent.")
+
+
 else:
     print("Emails were not sent since PDF file was not generated.")
--- send_mail.py
+++ send_mail.py
@@ -4,6 +4,8 @@ import smtplib
 from email.message import EmailMessage
 from typing import List, Union
 from variables import *
+import win32com.client as client
+
 
 
 def send_mail(send_from: str, send_to: list, subject: str, body: str, attachments : List[str], smtp_url : str, port : Union[int,str], username : str, password : str,starttls = True):
@@ -40,28 +42,49 @@ def send_mail(send_from: str, send_to: list, subject: str, body: str, attachment
     smtp.send_message(msg)
     smtp.quit()
 
+def check_emails_available_outlook(outlook:client.CDispatch,email:str)->bool:
+    if not outlook.Session.Accounts[email]:
+        print(f"{email}: NOT found on Outlook.")
+        return False    
+    return True
+
+
+
+def send_mail_outlook(outlook,send_from: str, send_to: list, subject: str, body: str, attachments : List[str]):
+    account = outlook.Session.Accounts[send_from]
+    # construct the email item object
+    message = outlook.CreateItem(0)
+    message.Subject = subject
+    message.Body = body
+    message.To = ";".join(send_to)
+    message._oleobj_.Invoke(*(64209, 0, 8, 0, account))
+    for attachment in attachments:
+        abs_path  = os. path. abspath(attachment)
+        print(abs_path)
+        message.Attachments.Add(abs_path)
+    message.Save()
+    message.Send()
+
 
 if __name__ == "__main__":
+
+    outlook = client.Dispatch('Outlook.Application')
+
 
     dict_conditions = {
         "always" : True,
     }
 
-
     for email in list_emails:
-        credentials = list_credentials[email['from']]
-        smtp_config = list_emails_providers[credentials['provider']]['smtp']
-        if dict_conditions[email['condition']]:
-            send_mail(
-                send_from = email['from'],
-                send_to = email['mailto'],
-                subject = email['subject'],
-                body = open(os.path.join(email_messages_path,email['text']),encoding="UTF-8").read(),
-                attachments = [list_pdf_files[0]],
-                smtp_url = smtp_config['server'],
-                port = smtp_config['port'],
-                username = email['from'], 
-                password = credentials['password']
-            )
-else:
-    print("Emails were not sent since PDF file was not generated.")
+        if check_emails_available_outlook(outlook, email['from']):
+            if dict_conditions[email['condition']]:
+                send_mail_outlook(
+                    outlook = outlook,
+                    send_from = email['from'],
+                    send_to = email['mailto'],
+                    subject = email['subject'],
+                    body = open(os.path.join(email_messages_path,email['text']),encoding="UTF-8").read(),
+                    attachments = [list_pdf_files[0]],
+                )
+                print(f"{email['from']}: File was sent.")
+    
